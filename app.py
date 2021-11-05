fbec02137e613f62f144083d839782c30bfa4229
derry derajat <derryderajat@mail.com>
2021-11-05 12:49:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ origins = [
     "http://127.0.0.1:8000/"
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:3000/",
+    "http://localhost:3000"
 ]
 
 app.add_middleware(
